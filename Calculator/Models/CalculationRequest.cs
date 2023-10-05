04b463aa186f144ccad5e33f3fc7c3b0396c1ba3
EFIF\aabd <aabd@aabc.dk>
2023-10-05 18:31:14 +0200

    Extend addition to accept multiple numbers

--- Calculator/Models/CalculationRequest.cs
+++ Calculator/Models/CalculationRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace Calculator.Models
+﻿using Calculator.Controllers;
+
+namespace Calculator.Models
 {
     public class CalculationRequest
     {
         public decimal Number1 { get; set; }
         public decimal Number2 { get; set; }
     }
+
 }
